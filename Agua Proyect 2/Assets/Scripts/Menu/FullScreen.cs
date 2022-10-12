830d91ef1f5180e632aa7b836679fd3ee527c278
sebastian6226 <94463840+sebastian6226@users.noreply.github.com>
2022-10-11 22:36:49 -0500

    menu de opciones

---
+++ Agua Proyect 2/Assets/Scripts/Menu/FullScreen.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class FullScreen : MonoBehaviour
+{
+    public Toggle toggle;
+
+    public TMP_Dropdown resolutionDropdown;
+
+    Resolution[] resolution;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Screen.fullScreen)
+        {
+            toggle.isOn = true;
+        }
+        else
+        {
+            toggle.isOn = false;
+        }
+
+        CheckResolution();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ActivateFullScreen(bool FullScreen)
+    {
+        Screen.fullScreen = FullScreen;
+    }
+
+    public void CheckResolution()
+    {
+        resolution = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        int resolucionActual = 0;
+
+        for (int i = 0; i < resolution.Lenght; i++)
+        {
+            string option = resolution[i].width + "x" + resolution[i].height;
+            options.Add(option);
+
+            if (Screen.fullScreen && resolution[i].width == Screen.currentResolution.width && resolution[i].height == Screen.currentResolution.height)
+            {
+                resolucionActual = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = resolucionActual;
+        resolutionDropdown.RefreshShownValue();
+
+        resolutionDropdown.value = PlayerPrefs.GetInt("numeroResolucion", 0);
+    }
+
+    public void ChangeResolution(int ResolutionIndex)
+    {
+        PlayerPrefs.SetInt("numeroResolucion", resolutionDropdown.value);
+
+        Resolution resolucion = resolution[ResolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+}
